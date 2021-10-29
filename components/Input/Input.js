f958969ea13dcc3a672be3e3a6d748063b45f590
ahmadbilalse <ahmadbilal.se@gmail.com>
2021-10-29 17:25:03 +0700

    add: input validation

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,22 +1,41 @@
 import React, { useState } from "react"
 import styles from './Input.module.css'
+import ipRegex from 'ip-regex'
+import classNames from "classnames";
 
 export default function Input() {
   const placeholder = "Search for any IP address or domain";
   const [inputText, setInputText] = useState('');
+  const [valid, setValid] = useState(true);
+
+  const isInputValid = (input) => {
+    const domainRegExp = /^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$/;
+    return ipRegex({ includeBoundaries: true }).test(input) || domainRegExp.test(input);
+  }
 
   const changeHandler = (e) => {
-    setInputText(e.target.value);
+    const input = e.target.value;
+    setInputText(input);
+    setValid(isInputValid(input));
   }
 
   const submitHandler = (e) => {
     e.preventDefault();
+    setValid(isInputValid(inputText));
   }
 
   return (
     <form className={styles.form}>
-      <input placeholder={placeholder} className={styles.inputText} value={inputText} type='text' onChange={changeHandler} />
-      <button className={styles.button} onClick={submitHandler}></button>
+      <input
+        required
+        placeholder={placeholder}
+        className={classNames({
+          [styles.inputText]: true,
+          [styles.inputText__invalid]: !valid,
+        })}
+        value={inputText} type='text'
+        onChange={changeHandler} />
+      <button disabled={!valid} className={styles.button} onClick={submitHandler}></button>
     </form>
   );
 }
